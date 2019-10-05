8fbce355196e030aca9c895c9c6ad2ba3501d7e9
TorokNiki <niki.torok@gmail.com>
2019-10-05 23:12:06 +0200

    File beolvasása

--- src/toto/szolgaltatas/TotoSzolgaltatas.java
+++ src/toto/szolgaltatas/TotoSzolgaltatas.java
@@ -15,59 +15,73 @@ import java.util.List;
 
 public class TotoSzolgaltatas {
     private List<Fordulo> fordulokLista;
-    public void beolvasas(String file){
-           this.fordulokLista= new ArrayList<>();
-            try{
-                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy.MM.d.");
-                int talatSzam=14;
-                FileReader fr= new FileReader(file);
-                BufferedReader br= new BufferedReader(fr);
-                String sor= br.readLine();
-                int sdfg=0;
-                while (sor!=null) {
-                    String[] adatok= sor.split(";");
-                    int ev = Integer.parseInt(adatok[0]);
-                    int het = Integer.parseInt(adatok[1]);
-                    int forduloHet = Integer.parseInt(adatok[2]);
-                   // adatok[3].replace('.','-');
-                    LocalDate datum= LocalDate.parse(adatok[3],formatter);
-                    List<Talalat> talalatokLita= new ArrayList<>();
-                    List<Eredmeny> eredmenyeklista= new ArrayList<>();
-                    for (int i = 0; i < 10; i+=2) {
-                        int nyertTalalatokSzama= Integer.parseInt(adatok[i+4]);
-                        String ujadat= adatok[i+5].replace(" Ft","");
-                        ujadat= ujadat.replace(" ","");
-                        int nyeremeny=Integer.parseInt(ujadat);
-                        talalatokLita.add(new Talalat(talatSzam,nyertTalalatokSzama,nyeremeny));
-                        talatSzam--;
-                    }
-                    for (int i = 0; i < 13+1; i++) {
-                        switch (adatok[i+6]){
-                            case "1": eredmenyeklista.add(Eredmeny._1); break;
-                            case  "2": eredmenyeklista.add(Eredmeny._2); break;
-                            case  "X": eredmenyeklista.add(Eredmeny.X); break;
-                        }
-                    }
-                    Fordulo fordulo= getFordulo(ev,het,forduloHet,datum,talalatokLita,eredmenyeklista);
-                    this.fordulokLista.add(fordulo);
-                    sor= br.readLine();
-                    sdfg = sdfg++;
+
+    public void beolvasas(String file) {
+        this.fordulokLista = new ArrayList<>();
+        try {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy.MM.d.");
+            int talatSzam = 14;
+            FileReader fr = new FileReader(file);
+            BufferedReader br = new BufferedReader(fr);
+            String sor = br.readLine();
+            while (sor != null) {
+                String[] adatok = sor.split(";");
+                int ev = Integer.parseInt(adatok[0]);
+                int het = Integer.parseInt(adatok[1]);
+                int forduloHet;
+                if (adatok[2].equals("")) {
+                    forduloHet = 1;
+                } else {
+                    forduloHet = Integer.parseInt(adatok[2]);
                 }
-                br.close();
-                fr.close();
-
-            }catch(FileNotFoundException ex){
-                System.err.printf("Hiba: %s",ex.getMessage());
-            }catch(IOException ex){
-                System.err.printf("Hiba: %s",ex.getMessage());
-            }catch(Exception e){
-                System.err.printf("Hiba: Exeption %s", e.getMessage());
+                // adatok[3].replace('.','-');
+                LocalDate datum;
+                if (adatok[3].equals("")) {
+                    datum = LocalDate.of(ev, 1, 1).plusWeeks(het).plusDays(forduloHet - 1);
+                } else {
+                    datum = LocalDate.parse(adatok[3], formatter);
+                }
+                List<Talalat> talalatokLita = new ArrayList<>();
+                List<Eredmeny> eredmenyeklista = new ArrayList<>();
+                for (int i = 0; i < 10; i += 2) {
+                    int nyertTalalatokSzama = Integer.parseInt(adatok[i + 4]);
+                    String ujadat = adatok[i + 5].replace(" Ft", "");
+                    ujadat = ujadat.replace(" ", "");
+                    int nyeremeny = Integer.parseInt(ujadat);
+                    talalatokLita.add(new Talalat(talatSzam, nyertTalalatokSzama, nyeremeny));
+                    talatSzam--;
+                }
+                for (int i = 0; i < 13 + 1; i++) {
+                    switch (adatok[i + 6]) {
+                        case "1":
+                            eredmenyeklista.add(Eredmeny._1);
+                            break;
+                        case "2":
+                            eredmenyeklista.add(Eredmeny._2);
+                            break;
+                        case "X":
+                            eredmenyeklista.add(Eredmeny.X);
+                            break;
+                    }
+                }
+                Fordulo fordulo = getFordulo(ev, het, forduloHet, datum, talalatokLita, eredmenyeklista);
+                this.fordulokLista.add(fordulo);
+                sor = br.readLine();
             }
+            br.close();
+            fr.close();
 
+        } catch (FileNotFoundException ex) {
+            System.err.printf("Hiba: %s", ex.getMessage());
+        } catch (IOException ex) {
+            System.err.printf("Hiba: %s", ex.getMessage());
+        } catch (Exception e) {
+            System.err.printf("Hiba: Exeption %s", e.getMessage());
+        }
     }
 
-    private Fordulo getFordulo(int ev,int het,int forduloHet,LocalDate datum,List<Talalat> talalatokLita,List<Eredmeny> eredmenyeklista) {
-        Fordulo fordulo= new Fordulo();
+    private Fordulo getFordulo(int ev, int het, int forduloHet, LocalDate datum, List<Talalat> talalatokLita, List<Eredmeny> eredmenyeklista) {
+        Fordulo fordulo = new Fordulo();
         fordulo.setEv(ev);
         fordulo.setHet(het);
         fordulo.setForduloAHeten(forduloHet);
